e55a54e0c7cec82310416f39727ed4853d33ad2a
Les Matheson <lmatheson4@bloomberg.net>
2019-11-19 16:09:18 -0500

    sync

---
+++ jinja1.py
@@ -0,0 +1,11 @@
+#!/opt/bb/bin/python3.7
+
+from jinja2 import Template
+
+t = Template("Hello {{something}}")
+
+print(t.render(something="World"))
+
+t = Template("Favorite numbers: {% for n in [1,2,3]%}{{n}} " " {% endfor %}")
+
+print(t.render())
