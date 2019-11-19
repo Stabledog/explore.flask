0955843289573a5ef2451bcec007cd78449236a1
Les Matheson <lmatheson4@bloomberg.net>
2019-11-19 16:58:54 -0500

    sync

--- run.py
+++ run.py
@@ -5,16 +5,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.context_processor
-def dbg_hook():
-    def set_trace():
-        import pdb
-        pdb.set_trace()
-    return dict(set_trace=set_trace)
+def set_trace():
+    import pudb
+    pudb.set_trace()
 
 @app.route("/")
 def template_test():
     return render_template('template.html', my_string="Wheee",my_list=[i for i in range(5)])
 
 if __name__=="__main__":
+    app.jinja_env.globals.update(set_trace=set_trace)
     app.run(host='0.0.0.0',port=9193, debug=True)
+
